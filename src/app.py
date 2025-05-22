0326e70af93a419b9762780390f15ce0242043bb
Dwane Dunn <dwane.dunn@gmail.com>
2025-05-22 14:56:53 +0000

    remove orphan '

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ def hello_world():
 
 @app.route('/todos', methods=['GET'])
 def hello():
-    return "<h1>'Hello!</h1>"
+    return "<h1>Hello!</h1>"
 
 # These two lines should always be at the end of your app.py file
 if __name__ == '__main__':
