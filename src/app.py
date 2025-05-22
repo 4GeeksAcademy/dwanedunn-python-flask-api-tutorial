dcfbfad247f326ab40c73778dd9e84cd5f371191
Dwane Dunn <dwane.dunn@gmail.com>
2025-05-22 14:48:55 +0000

    create /todos api

--- src/app.py
+++ src/app.py
@@ -7,6 +7,10 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route('/todos', methods=['GET'])
+def hello():
+    return "<h1>'Hello!</h1>"
+
 # These two lines should always be at the end of your app.py file
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
